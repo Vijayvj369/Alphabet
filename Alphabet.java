fc6d4a0017364999f6daacff300b3c50320c4247
mailbox.vj369 <mailbox.vj369@gmail.com>
2021-11-15 18:32:45 +0530

    descriptive comment for code change

---
+++ Alphabet.java
@@ -0,0 +1,11 @@
+public class Alphabet{
+    public static void main(String[] args){
+        
+        char alpha;
+        String str = ("a");
+        for(alpha = 'a'; alpha <= 'z'; alpha++){
+            System.out.println(alpha );
+        }
+        
+    }
+}
